e506d911f22d4c0ea654bf95279f5782048421b0
Elvia Pineda <elvipineda25616@gmail.com>
2023-09-07 21:58:01 -0500

    Chat

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  post 'add/user', to: 'rooms#add_user'
+  resources :rooms do
+    resources :messages
+  end
+  devise_for :users
+  root "rooms#index"
+ 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
